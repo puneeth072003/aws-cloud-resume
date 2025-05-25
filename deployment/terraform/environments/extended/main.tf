48b4c574a4c951a4649414e869b555dde5413998
Puneeth Y <pyd773@gmail.com>
2025-05-25 09:30:18 +0530

    feat: Rebuild Terraform configuration with new modules for AWS resources including CloudWatch, DynamoDB, IAM, Lambda, SNS, and outputs

---
+++ deployment/terraform/environments/extended/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "base_stack" {
+  source      = "../../modules/base"
+  alert_email = var.alert_email
+}
+
+module "extended_stack" {
+  source = "../../modules/extended"
+}
