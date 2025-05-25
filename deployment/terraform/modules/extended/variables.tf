ec95acf4bade81d1f0a2a5f80f9ee7e5a83e94df
Puneeth Y <pyd773@gmail.com>
2025-05-25 22:12:35 +0530

    feat: Add API Gateway and CloudFront configurations for frontend hosting; update outputs and variables

--- deployment/terraform/modules/extended/variables.tf
+++ deployment/terraform/modules/extended/variables.tf
@@ -7,3 +7,13 @@ variable "frontend_files_path" {
   description = "Path to the frontend files to be uploaded to S3"
   type        = string
 }
+
+variable "route53_zone_id" {
+  description = "The ID of the Route 53 hosted zone"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The domain name to associate with the CloudFront distribution"
+  type        = string
+}
